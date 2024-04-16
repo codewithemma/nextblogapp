0af3e182de458fb6b094800b48a13d064918ca4a
Chikezie Ugochukwu Emmanuel <codewithemma@gmail.com>
2024-04-16 14:37:16 +0100

    short break

--- src/app/posts/[slug]/page.jsx
+++ src/app/posts/[slug]/page.jsx
@@ -2,47 +2,58 @@ import Menu from "@/components/menu/Menu";
 import styles from "./SinglePage.module.css";
 import Image from "next/image";
 import Comments from "@/components/comments/Comments";
-const SinglePage = () => {
+const getData = async (slug) => {
+  const res = await fetch(`http://localhost:3000/api/posts/${slug}`, {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed");
+  }
+
+  return res.json();
+};
+const SinglePage = async ({ params }) => {
+  const { slug } = params;
+  const data = await getData(slug);
   return (
     <div className={styles.container}>
       <div className={styles.infoContainer}>
         <div className={styles.textContainer}>
-          <h1 className={styles.title}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          </h1>
+          <h1 className={styles.title}>{data?.title}</h1>
           <div className={styles.user}>
-            <div className={styles.userImageContainer}>
-              <Image src={"/p1.jpeg"} alt="" fill className={styles.avatar} />
-            </div>
+            {data?.user?.image && (
+              <div className={styles.userImageContainer}>
+                <Image
+                  src={data?.user?.image}
+                  alt=""
+                  fill
+                  className={styles.avatar}
+                />
+              </div>
+            )}
             <div className={styles.userTextContainer}>
-              <span className={styles.username}>John Doe</span>
-              <span className={styles.date}>98-38-3890</span>
+              <span className={styles.username}>{data?.user?.name}</span>
+              <span className={styles.date}>
+                {data?.createdAt.substring(0, 10)}
+              </span>
             </div>
           </div>
         </div>
-        <div className={styles.imageContainer}>
-          {" "}
-          <Image src={"/p1.jpeg"} alt="" fill className={styles.image} />
-        </div>
+        {data?.img && (
+          <div className={styles.imageContainer}>
+            <Image src={data.img} alt="" fill className={styles.image} />
+          </div>
+        )}
       </div>
       <div className={styles.content}>
         <div className={styles.post}>
-          <div className={styles.description}>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde ad
-              minima, ratione quis deserunt voluptatum dolores nam quos nobis
-              autem, provident harum porro illo, aperiam cumque obcaecati! A,
-              nulla quia.
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde ad
-              minima, ratione quis deserunt voluptatum dolores nam quos nobis
-              autem, provident harum porro illo, aperiam cumque obcaecati! A,
-              nulla quia.
-            </p>
-          </div>
+          <div
+            className={styles.description}
+            dangerouslySetInnerHTML={{ __html: data?.desc }}
+          />
           <div className={styles.comment}>
-            <Comments />
+            <Comments postSlug={slug} />
           </div>
         </div>
         <Menu />
--- src/components/comments/Comments.jsx
+++ src/components/comments/Comments.jsx
@@ -1,8 +1,26 @@
+"use client";
 import styles from "./Comments.module.css";
 import Link from "next/link";
 import Image from "next/image";
-const Comments = () => {
-  const status = "authenticated";
+import useSWR from "swr";
+import { useSession } from "next-auth/react";
+const fetcher = async (url) => {
+  const res = await fetch(url);
+  const data = await res.json();
+
+  if (!res.ok) {
+    const error = new Error(data.message);
+    throw error;
+  }
+
+  return data;
+};
+const Comments = ({ postSlug }) => {
+  const { status } = useSession();
+  const { data, isLoading } = useSWR(
+    `http://localhost:3000/api/comments?postSlug=${postSlug}`,
+    fetcher
+  );
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Comments</h1>
@@ -15,27 +33,30 @@ const Comments = () => {
         <Link href={"/login"}>Login to write a comment</Link>
       )}
       <div className={styles.comments}>
-        <div className={styles.comment}>
-          <div className={styles.user}>
-            <Image
-              src={"/p1.jpeg"}
-              alt=""
-              width={50}
-              height={50}
-              className={styles.image}
-            />
-            <div className={styles.userInfo}>
-              <span className={styles.username}>John Doe</span>
-              <span className={styles.username}>83-39-2234</span>
-            </div>
-          </div>
-          <p className={styles.desc}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui
-            perferendis voluptate quidem facilis hic! Voluptates, adipisci
-            porro, provident ullam enim deserunt dolorum qui sit nesciunt
-            consequatur quisquam necessitatibus laudantium?
-          </p>
-        </div>
+        {isLoading
+          ? "loading"
+          : data?.map((item) => {
+              return (
+                <div className={styles.comment} key={item._id}>
+                  <div className={styles.user}>
+                    {item?.user?.image && (
+                      <Image
+                        src={item.user.image}
+                        alt=""
+                        width={50}
+                        height={50}
+                        className={styles.image}
+                      />
+                    )}
+                    <div className={styles.userInfo}>
+                      <span className={styles.username}>{item.user.name}</span>
+                      <span className={styles.username}>{item.createdAt}</span>
+                    </div>
+                  </div>
+                  <p className={styles.desc}>{item.desc}</p>
+                </div>
+              );
+            })}
       </div>
     </div>
   );
