45a4cc03df3ae96d96bf0e7698431f85319041f8
Chikezie Ugochukwu Emmanuel <codewithemma@gmail.com>
2024-04-18 15:33:05 +0100

    e no work

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -1,31 +1,5 @@
 "use client";
-// import React, { createContext, useEffect, useState } from "react";
-// export const ThemeContext = createContext();
-// const getFromLocalStorage = () => {
-//   if (typeof window !== "undefined") {
-//     const value = localStorage.getItem("theme");
-//     return value || "light";
-//   }
-// };
-// export const ThemeContextProvider = ({ children }) => {
-//   const [theme, setTheme] = useState(() => {
-//     return getFromLocalStorage();
-//   });
-
-//   const toggle = () => {
-//     setTheme(theme === "light" ? "dark" : "light");
-//   };
-//   useEffect(() => {
-//     localStorage.setItem("theme", theme);
-//   }, [theme]);
-
-//   return (
-//     <ThemeContext.Provider value={{ theme, toggle }}>
-//       {children}
-//     </ThemeContext.Provider>
-//   );
-// };
-import { useState, useEffect, createContext } from "react";
+import React, { createContext, useEffect, useState } from "react";
 export const ThemeContext = createContext();
 const getFromLocalStorage = () => {
   if (typeof window !== "undefined") {
@@ -41,7 +15,6 @@ export const ThemeContextProvider = ({ children }) => {
   const toggle = () => {
     setTheme(theme === "light" ? "dark" : "light");
   };
-
   useEffect(() => {
     localStorage.setItem("theme", theme);
   }, [theme]);
